f8177f54dae6ae3e9a2f0e609e7898b5bd23a085
he_ubuntu <gyai325sune@gmail.com>
2024-10-09 21:57:00 +0900

    2024/10/09

---
+++ chap1/1.3/list1.4/list1.4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int i = 42;
+
+    std::cout << "++i:  " << ++i << std::endl;
+
+    std::cout << "i:    " << i << std::endl;
+
+    std::cout << "i++:  " << i++ << std::endl;
+
+    std::cout << "i:    " << i << std::endl;
+
+}
---
+++ chap1/1.4/list1.7/list1.7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int a = 11;
+    if(10 < a)
+        std::cout << "変数aは10より大きいです" << std::endl;
+    
+    std::cout << "if文のあと" << std::endl;
+}
